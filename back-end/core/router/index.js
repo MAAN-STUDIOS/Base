6470b92aca6fb8da9ae096511191fd641536ec22
Katia Alvarez <A01781097@tec.mx>
2025-05-29 20:38:49 -0600

    ADD: Endpoint to view the configuration

--- back-end/core/router/index.js
+++ back-end/core/router/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import get_logger from '../utils/logger.js';
 import { UserController } from "../controllers/user.js";
+import { ConfigController } from '../controllers/config.js';
 
 
 const router = express.Router();
@@ -8,5 +9,6 @@ const logger = get_logger('GameRouter');
 
 
 router.get('/user/:id', UserController.get_user);
+router.get('/config/:id', ConfigController.get_config);
 
 export { router };
