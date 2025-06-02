1de5dc208a9d4b762e28e7d3a218ce2aa9db3beb
A01781097 <A01781097@tec.mx>
2025-06-02 13:11:30 -0600

    ADD: Endpoint to view the player info (#147)

--- back-end/core/router/index.js
+++ back-end/core/router/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import get_logger from '../utils/logger.js';
 
+import { UserController } from "../controllers/user.js";
+import { PlayerController } from '../controllers/player.js';
 import { AuthController } from '../controllers/auth.js';
-import { UserController }  from '../controllers/user.js';
 import { GameController } from '../controllers/game.js';
 
 //import { GameController } from '../controllers/game';
@@ -13,11 +14,11 @@ const logger = get_logger('GameRouter');
 
 
 router.get('/user/:id', UserController.get_user);
+router.get('/player/:id', PlayerController.get_player);
 router.post('/auth/login', AuthController.login);
 router.post('/auth/register', AuthController.register);
 router.post('/auth/verify', AuthController.verifyToken);
 router.post('/auth/refresh', AuthController.refreshAccessToken);
 router.get('/game/start/:name/:description/:seed', GameController.startGame)
-
 
 export { router };
