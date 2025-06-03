22861cb99c27bfcd806e9f6c1004e3f7b5058d43
Katia Alvarez <A01781097@tec.mx>
2025-06-03 17:25:31 -0600

    FIX: correct view

--- back-end/core/controllers/viewGame.js
+++ back-end/core/controllers/viewGame.js
@@ -3,7 +3,7 @@ import db from '../../config/db.js';
 export class ViewGameController{
     static async get_viewGame(req, res){
         try {
-            const data = await db.query('SELECT * FROM view_human_view_game');
+            const data = await db.query('SELECT * FROM view_game');
             res.json(data);
         } catch (error) {
             console.error('Error in get_viewGame:', error);
