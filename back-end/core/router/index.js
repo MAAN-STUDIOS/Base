070a13876dc645f2cbb61e560b7fa6430265ba14
A01781097 <A01781097@tec.mx>
2025-06-04 13:26:19 -0600

    ADD: endpoint to check the view of the game in human edition (#154)

--- back-end/core/router/index.js
+++ back-end/core/router/index.js
@@ -7,8 +7,10 @@ import { ConfigController } from '../controllers/config.js';
 import { PlayerController } from '../controllers/player.js';
 import { AuthController } from '../controllers/auth.js';
 import { GameController } from '../controllers/game.js';
+import { HumanGame } from '../controllers/viewHumanGame.js';
 import { HumanGame } from '../controllers/viewFloodGame.js';
 import { ViewGameController } from '../controllers/viewGame.js';
+
 
 const router = express.Router();
 const logger = get_logger('GameRouter');
@@ -85,6 +87,7 @@ logger.info('Game router initialized with all endpoints');
 
 router.get('/api/view/game', ViewGameController.get_viewGame);
 router.get('/api/view/flood_view_game', HumanGame.get_viewFloodGame);
+router.get('/api/view/human_view_game', HumanGame.get_viewHumanGame);
 
 logger.info('Game router initialized with all endpoints');
 
