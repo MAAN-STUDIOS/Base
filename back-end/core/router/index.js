c11c8c0942c59903bf13910e1a9bd20bc47b2fab
A01781097 <A01781097@tec.mx>
2025-06-04 13:28:41 -0600

    ADD: endpoint to check the view of the layout (#156)

--- back-end/core/router/index.js
+++ back-end/core/router/index.js
@@ -7,6 +7,7 @@ import { ConfigController } from '../controllers/config.js';
 import { PlayerController } from '../controllers/player.js';
 import { AuthController } from '../controllers/auth.js';
 import { GameController } from '../controllers/game.js';
+import { ViewLayout } from '../controllers/viewLayout.js';
 import { viewChunk } from '../controllers/viewChunk.js';
 import { HumanGame } from '../controllers/viewHumanGame.js';
 import { HumanGame } from '../controllers/viewFloodGame.js';
@@ -89,6 +90,7 @@ router.get('/api/view/game', ViewGameController.get_viewGame);
 router.get('/api/view/flood_view_game', HumanGame.get_viewFloodGame);
 router.get('/api/view/human_view_game', HumanGame.get_viewHumanGame);
 router.get('/api/view/chunk', viewChunk.get_viewChunk);
+router.get('/api/view/layout', ViewLayout.get_viewLayout);
 
 logger.info('Game router initialized with all endpoints');
 
