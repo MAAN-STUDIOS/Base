dfebeaee1fb933519291d0439c375a73de0243c4
AngelMontemayorDavila <149808501+AngelMontemayorDavila@users.noreply.github.com>
2025-06-05 15:40:15 -0600

    Context screen ready (#187)
    Co-authored-by: Emiliano20Deloso <emilianodelgadilloosorio1@outlook.com>

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -14,6 +14,7 @@ import screenLogin from "@screens/login.js";
 import screenCreateAccount from "@screens/createAccount.js";
 import screenJoinGame from "@screens/joinGame.js";
 import screenDashboard from "@screens/dashboard.js";
+import context_screen from '@screens/context_screen';
 
 // Register all screens
 registerScreen('menu', screenMenu);
@@ -24,6 +25,9 @@ registerScreen(404, screenPageNotFound);
 registerScreen('credits', screenCredits);
 registerScreen('login', screenLogin);
 registerScreen('create-account', screenCreateAccount);
+registerScreen('dashboard', screenDashboard);
+registerScreen('context_screen', context_screen);
+
 registerScreen('join-game', screenJoinGame);
 registerScreen('dashboard', screenDashboard);
 
@@ -62,4 +66,3 @@ initAudio();
 
 // Navigate to the appropriate screen
 navigate(location.pathname.slice(1) || 'menu');
-
