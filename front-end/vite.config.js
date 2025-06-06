e31f2aa6ac9c4e13fe19f409052a382b860def8c
AngelMontemayorDavila <149808501+AngelMontemayorDavila@users.noreply.github.com>
2025-06-06 16:49:39 -0600

    Fr 000 add env on front end (#191)
    * Upda player size
    
    * Update frontend to handle .env

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,32 +1,59 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import fs from 'fs';
+import dotenv from 'dotenv';
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src/'),
-      '@utils': path.resolve(__dirname, './src/core/utils/'),
-      '@engine': path.resolve(__dirname, './src/core/engine/'),
-      '@screens': path.resolve(__dirname, './src/screens/'),
-      '@assets': path.resolve(__dirname, './src/assets/'),
-      '@sprites': path.resolve(__dirname, './src/assets/sprites/'),
-      '@tiles': path.resolve(__dirname, './src/assets/tiles/')
+
+export default defineConfig(({ mode }) => {
+    let envFile = '.env.dev';
+
+    if (mode === 'production') {
+        envFile = '.env.production';
+    } else if (!fs.existsSync(path.resolve(__dirname, envFile))) {
+        envFile = '.env.develop';
     }
-  },
-  css: {
-    modules: {
-      scopeBehaviour: 'local',
-      localsConvention: 'camelCase',
-      generateScopedName: '[name]__[local]___[hash:base64:5]',
+
+    let env = {};
+    if (fs.existsSync(path.resolve(__dirname, envFile))) {
+        console.log(`Loading environment from ${envFile}`);
+        const result = dotenv.config({ path: path.resolve(__dirname, envFile) });
+        if (result.parsed) {
+            env = result.parsed;
+        }
     }
-  },
-  server: {
-    proxy: {
-      '/socket.io': {
-        target: "http://localhost:8000",
-        ws: true,
-        changeOrigin: true
-      }
-    }
-  }
+
+    const backendUrl = env.BACKEND_URL.replace(/\/+$/, '');
+
+    return {
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src/'),
+                '@utils': path.resolve(__dirname, './src/core/utils/'),
+                '@engine': path.resolve(__dirname, './src/core/engine/'),
+                '@screens': path.resolve(__dirname, './src/screens/'),
+                '@assets': path.resolve(__dirname, './src/assets/'),
+                '@sprites': path.resolve(__dirname, './src/assets/sprites/'),
+                '@tiles': path.resolve(__dirname, './src/assets/tiles/')
+            }
+        },
+        css: {
+            modules: {
+                scopeBehaviour: 'local',
+                localsConvention: 'camelCase',
+                generateScopedName: '[name]__[local]___[hash:base64:5]',
+            }
+        },
+        server: {
+            proxy: {
+                '/socket.io': {
+                    target: backendUrl,
+                    ws: true,
+                    changeOrigin: true
+                }
+            }
+        },
+        define: {
+            'process.env': env
+        }
+    };
 });
