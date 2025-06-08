b37c16ef844b9b72475826d40b2641852c44534b
Katia Alvarez <A01781097@tec.mx>
2025-06-07 19:09:39 -0600

    Basis to try sprites

--- front-end/src/core/engine/otherEnemy.js
+++ front-end/src/core/engine/otherEnemy.js
@@ -11,75 +11,75 @@ export class OtherEnemy {
         this.height = 32;
         this.enemyType = type;
 
-        this.img = new Image();
-        this.img.src = SpriteSheet;
+        // this.img = new Image();
+        // this.img.src = SpriteSheet;
 
-        this.spriteRect = new Rect(0, 0, 153, 153);
-        this.previousDirection = "down";
-        this.currentDirection = "down";
-        this.frame = 0;
-        this.minFrame = 0;
-        this.maxFrame = 0;
-        this.repeat = true;
-        this.frameDuration = 100;
-        this.totalTime = 0;
-        this.sheetCols = 6;
+        // this.spriteRect = new Rect(0, 0, 153, 153);
+        // this.previousDirection = "down";
+        // this.currentDirection = "down";
+        // this.frame = 0;
+        // this.minFrame = 0;
+        // this.maxFrame = 0;
+        // this.repeat = true;
+        // this.frameDuration = 100;
+        // this.totalTime = 0;
+        // this.sheetCols = 6;
 
     }
 
-    setAnimation(minFrame, maxFrame, repeat, duration) {
-        this.minFrame = minFrame;
-        this.maxFrame = maxFrame;
-        this.frame = minFrame;
-        this.repeat = repeat;
-        this.totalTime = 0;
-        this.frameDuration = duration * 1000;
-    }
+    // setAnimation(minFrame, maxFrame, repeat, duration) {
+    //     this.minFrame = minFrame;
+    //     this.maxFrame = maxFrame;
+    //     this.frame = minFrame;
+    //     this.repeat = repeat;
+    //     this.totalTime = 0;
+    //     this.frameDuration = duration * 1000;
+    // }
 
-    updateFrame(deltaTime) {
-        this.totalTime += deltaTime;
+    // updateFrame(deltaTime) {
+    //     this.totalTime += deltaTime;
 
-        if (this.totalTime > this.frameDuration) {
-            const restartFrame = this.repeat ? this.minFrame : this.frame;
-            this.frame = this.frame < this.maxFrame ? this.frame + 1 : restartFrame;
+    //     if (this.totalTime > this.frameDuration) {
+    //         const restartFrame = this.repeat ? this.minFrame : this.frame;
+    //         this.frame = this.frame < this.maxFrame ? this.frame + 1 : restartFrame;
 
-            this.spriteRect.x = this.frame % this.sheetCols;
-            this.spriteRect.y = Math.floor(this.frame / this.sheetCols);
+    //         this.spriteRect.x = this.frame % this.sheetCols;
+    //         this.spriteRect.y = Math.floor(this.frame / this.sheetCols);
 
-            this.totalTime = 0;
-        }
-    }
+    //         this.totalTime = 0;
+    //     }
+    // }
 
-    setMovementAnimation() {
-        const stateAnimations = this.enemyType === 'flood'
-            ? {
-                'IDLE': floodEnemy.idle,
-                'PURSUE': floodEnemy.pursue,
-                'SEARCH': floodEnemy.search,
-                'ATTACK': floodEnemy.attack,
-                'RETREAT': floodEnemy.retreat
-            }
-            : {
-                'IDLE': humanEnemy.idle,
-                'PURSUE': humanEnemy.pursue,
-                'SEARCH': humanEnemy.search,
-                'ATTACK': humanEnemy.attack,
-                'RETREAT': humanEnemy.retreat
-            };
+    // setMovementAnimation() {
+    //     const stateAnimations = this.enemyType === 'flood'
+    //         ? {
+    //             'IDLE': floodEnemy.idle,
+    //             'PURSUE': floodEnemy.pursue,
+    //             'SEARCH': floodEnemy.search,
+    //             'ATTACK': floodEnemy.attack,
+    //             'RETREAT': floodEnemy.retreat
+    //         }
+    //         : {
+    //             'IDLE': humanEnemy.idle,
+    //             'PURSUE': humanEnemy.pursue,
+    //             'SEARCH': humanEnemy.search,
+    //             'ATTACK': humanEnemy.attack,
+    //             'RETREAT': humanEnemy.retreat
+    //         };
 
-        const anim = stateAnimations[this.state];
+    //     const anim = stateAnimations[this.state];
 
-        if (anim && this.currentDirection !== this.state) {
-            this.setAnimation(...anim.frames, anim.repeat, anim.duration);
-            this.currentDirection = this.state;
-        }
-    }
+    //     if (anim && this.currentDirection !== this.state) {
+    //         this.setAnimation(...anim.frames, anim.repeat, anim.duration);
+    //         this.currentDirection = this.state;
+    //     }
+    // }
 
     update() {
         this.position.lerpEqual(this.target, 1.0);
 
-        this.setMovementAnimation();
-        this.updateFrame(dt * 1000);
+        // this.setMovementAnimation();
+        // this.updateFrame(dt * 1000);
     }
 
     draw(ctx, playerPosition, cameraWidth, cameraHeight) {
@@ -95,20 +95,20 @@ export class OtherEnemy {
 
         if (isNotVisible) return;
 
-        ctx.drawImage(
-            this.img,
-            this.spriteRect.x * this.spriteRect.width,
-            this.spriteRect.y * this.spriteRect.height,
-            this.spriteRect.width,
-            this.spriteRect.height,
-            screenX - this.width / 2,
-            screenY - this.height / 2,
-            this.width,
-            this.height
-        );
+        // ctx.drawImage(
+        //     this.img,
+        //     this.spriteRect.x * this.spriteRect.width,
+        //     this.spriteRect.y * this.spriteRect.height,
+        //     this.spriteRect.width,
+        //     this.spriteRect.height,
+        //     screenX - this.width / 2,
+        //     screenY - this.height / 2,
+        //     this.width,
+        //     this.height
+        // );
 
-        //ctx.fillStyle = this.getStateColor();
-        //ctx.fillRect(screenX - this.width / 2, screenY - this.height / 2, this.width, this.height);
+        ctx.fillStyle = this.getStateColor();
+        ctx.fillRect(screenX - this.width / 2, screenY - this.height / 2, this.width, this.height);
     }
 
     getStateColor() {
